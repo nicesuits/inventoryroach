b2bcfd677290c6d4a3b8ff054279552584c581b7
nicesuits <raion314@outlook.com>
2019-04-18 15:17:31 -0700

    convert to stateless function

--- frontend/src/components/IssueTable.js
+++ frontend/src/components/IssueTable.js
@@ -2,28 +2,26 @@ import React, { Component } from 'react';
 import IssueRow from './IssueRow';
 import './IssueTable.css';
 
-class IssueTable extends Component {
-  render() {
-    const issueRows = this.props.issues.map(issue => (
-      <IssueRow key={issue.id} issue={issue} />
-    ));
-    return (
-      <table>
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Status</th>
-            <th>Owner</th>
-            <th>Created</th>
-            <th>Effort</th>
-            <th>Completion Date</th>
-            <th>Title</th>
-          </tr>
-        </thead>
-        <tbody>{issueRows}</tbody>
-      </table>
-    );
-  }
+function IssueTable(props) {
+  const issueRows = props.issues.map(issue => (
+    <IssueRow key={issue.id} issue={issue} />
+  ));
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Status</th>
+          <th>Owner</th>
+          <th>Created</th>
+          <th>Effort</th>
+          <th>Completion Date</th>
+          <th>Title</th>
+        </tr>
+      </thead>
+      <tbody>{issueRows}</tbody>
+    </table>
+  );
 }
 
 export default IssueTable;
