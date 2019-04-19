bf8fa1c6c26d9be5945665eee4ee6b4f1081dde4
nicesuits <raion314@outlook.com>
2019-04-18 17:03:59 -0700

    add logging

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,25 @@
 const Koa = require('koa');
 const Router = require('koa-router');
+const logger = require('koa-logger');
+
 const app = new Koa();
 const router = new Router();
+
+app.use(logger());
+app.use(router.routes());
+app.use(router.allowedMethods());
+app.use(async (ctx, next) => {
+  try {
+    await next();
+  } catch (err) {
+    ctx.status = err.status || 500;
+    ctx.body = err.message;
+    ctx.app.emit('error', err, ctx);
+  }
+});
 
 router.get('/', (ctx, next) => {
   ctx.body = 'Hello world';
 });
 
-app.use(router.routes());
-app.use(router.allowedMethods());
 app.listen(3000);
