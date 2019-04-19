9d91466b2e95cb24a0c9113506d697635f9c3d80
nicesuits <raion314@outlook.com>
2019-04-19 00:09:16 -0700

    add post new issue

--- backend/routes/issues.js
+++ backend/routes/issues.js
@@ -1,24 +1,34 @@
+const issues = [
+  {
+    id: 1,
+    status: 'Open',
+    owner: 'Ravan',
+    created: new Date('2016-08-15'),
+    effort: 5,
+    completionDate: undefined,
+    title: 'Error in console when clicking Add'
+  },
+  {
+    id: 2,
+    status: 'Assigned',
+    owner: 'Eddie',
+    created: new Date('2016-08-16'),
+    effort: 14,
+    completionDate: new Date('2016-08-30'),
+    title: 'Missing bottom border on panel'
+  }
+];
+
 module.exports = ({ issuesRouter }) => {
   issuesRouter.get('/issues', (ctx, next) => {
-    ctx.body = [
-      {
-        id: 1,
-        status: 'Open',
-        owner: 'Ravan',
-        created: new Date('2016-08-15'),
-        effort: 5,
-        completionDate: undefined,
-        title: 'Error in console when clicking Add'
-      },
-      {
-        id: 2,
-        status: 'Assigned',
-        owner: 'Eddie',
-        created: new Date('2016-08-16'),
-        effort: 14,
-        completionDate: new Date('2016-08-30'),
-        title: 'Missing bottom border on panel'
-      }
-    ];
+    ctx.body = issues;
+  });
+  issuesRouter.post('/issues', (ctx, next) => {
+    const newIssue = ctx.request.body;
+    newIssue.id = issues.length + 1;
+    newIssue.created = new Date();
+    if (!newIssue.status) newIssue.status = 'New';
+    issues.push(newIssue);
+    ctx.body = issues;
   });
 };
