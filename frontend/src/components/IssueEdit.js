fe4b39c97660cd21c85a8ec8e8a53997f24c7dad
nicesuits <raion314@outlook.com>
2019-04-20 21:43:40 -0700

    fix return statement

--- frontend/src/components/IssueEdit.js
+++ frontend/src/components/IssueEdit.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const IssueEdit = () => {
-  <div>This is a placeholder for the Issue Edit page.</div>;
+  return <div>This is a placeholder for the Issue Edit page.</div>;
 };
 
 export default IssueEdit;
