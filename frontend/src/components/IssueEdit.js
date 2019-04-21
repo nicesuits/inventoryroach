c932ba6760e3963d9cb9d1eda8aaef95c26e25b0
nicesuits <raion314@outlook.com>
2019-04-20 22:38:39 -0700

    update placeholder. add link to home

--- frontend/src/components/IssueEdit.js
+++ frontend/src/components/IssueEdit.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const IssueEdit = () => {
-  return <div>This is a placeholder for the Issue Edit page.</div>;
+const IssueEdit = props => {
+  return (
+    <div>
+      <p>This is a placeholder for editing issue {props.match.params.id}.</p>
+      <Link to="/issues">Back to issue list</Link>
+    </div>
+  );
 };
 
 export default IssueEdit;
