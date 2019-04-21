f5c3e1236923f1aa76afad19688e78f28e24abe0
nicesuits <raion314@outlook.com>
2019-04-20 21:31:46 -0700

    add completion_date display

--- frontend/src/components/IssueRow.js
+++ frontend/src/components/IssueRow.js
@@ -3,14 +3,14 @@ import React from 'react';
 const IssueRow = props => {
   return (
     <tr>
-      <td>{props.issue.id}</td>
+      <td>{props.issue.id.substring(10)}</td>
       <td>{props.issue.status}</td>
       <td>{props.issue.owner}</td>
       <td>{props.issue.created.toDateString()}</td>
       <td>{props.issue.effort}</td>
       <td>
-        {props.issue.completionDate
-          ? props.issue.completionDate.toDateString()
+        {props.issue.completion_date
+          ? new Date(props.issue.completion_date).toDateString()
           : ''}
       </td>
       <td>{props.issue.title}</td>
