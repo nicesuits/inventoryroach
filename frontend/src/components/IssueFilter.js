14e3c27eff7ead7fd440f2d2fb273747d3bd6d8c
nicesuits <raion314@outlook.com>
2019-04-21 16:42:39 -0700

    add params

--- frontend/src/components/IssueFilter.js
+++ frontend/src/components/IssueFilter.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const IssueFilter = () => {
-  return <div>This is a placeholder for IssueFilter.</div>;
+  const Seperator = () => <span> | </span>;
+  return (
+    <div>
+      <Link to="/issues">All Issues</Link>
+      <Seperator />
+      <Link to={{ pathname: '/issues', query: { status: 'Open' } }}>
+        Open Issues
+      </Link>
+      <Seperator />
+      <Link to={{ pathname: '/issues', query: { status: 'Assigned' } }}>
+        Assigned Issues
+      </Link>
+    </div>
+  );
 };
 
 export default IssueFilter;
